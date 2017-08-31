9ac5b6b506346404efa0eb243694c46f9bd7bb4f
Wouter ibens <wouter.ibens@nokia.com>
2017-08-31 11:29:05 -0400

    Add a command to list/jump to the errors

--- pyflakeslw.py
+++ pyflakeslw.py
@@ -10,6 +10,8 @@ from threading import Timer
 class PyFlakesLwListener(sublime_plugin.EventListener):
     REGION_KEY = 'pyflakeslw_errors'
     STATUS_KEY = 'pyflakeslw_status'
+    SETTINGS_KEY = 'pyflakeslw_errors'
+    current_errors = {}
 
     def __init__(self):
         self.errors={}
@@ -21,8 +23,8 @@ class PyFlakesLwListener(sublime_plugin.EventListener):
             return
         lang, _ = view.scope_name(view.sel()[0].begin()).split(' ', 1)
         if not lang == "source.python":
+            self.view.settings().erase(self.SETTINGS_KEY)
             return
-        print("Restarting timer!")
         self.notify.cancel()
         self.notify = Timer(0.3, lambda: self.update())
         self.view = view
@@ -66,4 +68,30 @@ class PyFlakesLwListener(sublime_plugin.EventListener):
         self.view.erase_regions(self.REGION_KEY)
         self.view.add_regions(self.REGION_KEY, regions, 'invalid', 'circle')
         self.on_selection_modified(self.view)
+        PyFlakesLwListener.current_errors = self.errors
+        self.view.settings().set(self.SETTINGS_KEY, "True")
 
+class PyFlakesLwCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        view = self.window.active_view()
+        if not view.settings().get(PyFlakesLwListener.SETTINGS_KEY):
+            return
+        if not view.sel():
+            return
+        lang, _ = view.scope_name(view.sel()[0].begin()).split(' ', 1)
+        if not lang == "source.python":
+            self.view.settings().erase(self.SETTINGS_KEY)
+            return
+        self.keys = list(PyFlakesLwListener.current_errors.keys())
+        self.keys.sort()
+        items = ["L{}: {}".format(lineno, PyFlakesLwListener.current_errors[lineno]) for lineno in self.keys]
+        self.window.show_quick_panel(items, self.go)
+
+    def go(self, i):
+        view = self.window.active_view()
+        lineno = self.keys[i]
+        point = view.text_point(lineno-1, 0)
+        r = view.line(point)
+        view.show_at_center(r)
+        view.sel().clear()
+        view.sel().add(r.end())
